426e4520bb3273365108b223d02d8a492382b79a
rishabh dev <rishabhd921@gmail.com>
2023-10-03 22:47:27 +0530

    update

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -29,7 +29,7 @@ const ProductDetails = () => {
                         <h3 className='title'>{product.title}</h3>
                         <p className='category'>{product.category}</p>
                         <h3 className='price'>${product.price}</h3>
-                        <p>{product.description}</p>
+                        <p>{product.description.substring(0,50) + "..."}</p>
                     </div>
                 </div>
 
